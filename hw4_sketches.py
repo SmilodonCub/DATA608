4ece092f7efbfebd42de365f52bbb549e989a003
SmilodonCub <smilodon138@gmail.com>
2021-10-22 15:12:42 -0400

    checks all the boxes.

--- hw4_sketches.py
+++ hw4_sketches.py
@@ -44,7 +44,6 @@ print( tree_dat_4Qs.head() )
 # needle direction (helper for draw_boro_dial)
 def needle_dir( boro_prop ):
     num_rows = boro_prop.shape[0]
-    ticks = []
     if num_rows == 3:
         ranges = [ [0, boro_prop[0]], 
              [boro_prop[0], boro_prop[0] + boro_prop[1]],
@@ -128,12 +127,49 @@ def draw_boro_dial( boro_health_counts, boro ):
     
     return fig
 
+def draw_dial_legend():
+    # draw simple legend for dial plots
+    fig = go.Figure()
+    # Create scatter trace of text labels
+    fig.add_trace(go.Scatter(
+        x=[15, 15, 15],
+        y=[4, 12, 20],
+        text=["Poor","Fair","Good"],
+        mode="text" ))
+    # Set axes properties
+    fig.update_xaxes(range=[0, 24])
+    fig.update_yaxes(range=[0, 24])
+    # Add shapes
+    fig.add_shape(type="rect",
+                  xref="paper", yref="paper",
+                  x0=3/12, y0=1/12,
+                  x1=5/12, y1=3/12,
+                  line=dict(color="gray",width=3),
+                  fillcolor='gold')
+    fig.add_shape(type="rect",
+                  xref="paper", yref="paper",
+                  x0=3/12, y0=5/12,
+                  x1=5/12, y1=7/12,
+                  line=dict(color="gray",width=3),
+                  fillcolor='greenyellow')
+    fig.add_shape(type="rect",
+                  xref="paper", yref="paper",
+                  x0=3/12, y0=9/12,
+                  x1=5/12, y1=11/12,
+                  line=dict(color="gray",width=3),
+                  fillcolor='forestgreen')
+    fig.update_layout(paper_bgcolor = "cornsilk", font = {'color': "gray", 'family': "Arial", 'size':36},
+                      xaxis={'showgrid': False, 'showticklabels':False},
+                      yaxis={'showgrid': False, 'showticklabels':False},
+                      plot_bgcolor='rgba(0,0,0,0)')
+    return fig
+
 # choropleth map of trees per selected species
 def map_certain_trees( df_species ):
     NY_coords = [ 40.7128, -74.0060 ]
     ny_map = folium.Map( location = NY_coords, tiles = 'Stamen Watercolor', zoom_start = 10 )
     health_class = [ 'Poor', 'Fair', 'Good' ]
-    class_color = [ 'red', 'orange', 'green' ]
+    class_color = [ 'orange', 'lightgreen', 'green' ]
     marker_cluster = MarkerCluster().add_to( ny_map )
     for num,hclass in enumerate(health_class):
         df_species_class = df_species[ df_species[ 'health' ] == hclass ]
@@ -144,6 +180,71 @@ def map_certain_trees( df_species ):
     ny_map.save( 'nycmap.html' )
     
 
+def steward_impact_data( df_species ):
+    #helper function to clean data
+    
+    #get counts grouped by boro, health & stewards
+    df_stewards = df_species.groupby(['boroname', 'health']).steward.value_counts()
+    df_stewards.name = 'steward_x_health_counts'
+    df_stewards = df_stewards.reset_index()
+    #get counts grouped by boro & steward
+    df_healthbn = df_species.groupby(['boroname']).steward.value_counts()
+    df_healthbn.name = 'steward_counts'
+    df_healthbn.reset_index()
+    #merge counts
+    df_stewards = df_stewards.merge( df_healthbn, on = ['boroname', 'steward'])
+    df_stewards['stewards_percent'] = round( 100*df_stewards['steward_x_health_counts']/df_stewards['steward_counts'],2 )
+    df_stewards.steward = pd.Categorical(df_stewards.steward, 
+                      categories=["None","1or2","3or4","4orMore"],
+                      ordered=True)
+    df_stewards = df_stewards.sort_values( ['steward', 'health']).reset_index()
+    return df_stewards
+
+def steward_impact_plot( df_steward ):
+    fig = go.Figure()
+
+    fig.update_layout(
+        template="simple_white",
+        title = 'Relative Proportion of Trees in Good Health',
+        title_x=0.5,
+        xaxis=dict(title_text="Number of Stewards grouped by NYC Borough"),
+        yaxis=dict(title_text="%"),
+        barmode="stack",
+        paper_bgcolor = "cornsilk", 
+        font = {'color': "gray", 'family': "Arial", 'size':18},
+        plot_bgcolor='rgba(0,0,0,0)')
+
+    colors = ["gold", "greenyellow", "forestgreen"]
+    health_lev = ['Poor', 'Fair', 'Good']
+
+    for r, c in zip(health_lev, colors):
+        plot_df = df_steward[df_steward['health'] == r]
+        fig.add_trace( go.Bar(x=[plot_df.boroname, plot_df.steward], 
+                              y=plot_df.stewards_percent, name=r, marker_color=c) )
+    return fig
+
+def steward_numerosity_plot( df_steward ):
+    fig = go.Figure()
+
+    fig.update_layout(
+        template="simple_white",
+        title = 'Numerosity of Trees',
+        title_x=0.5,
+        xaxis=dict(title_text="Number of Stewards grouped by NYC Borough"),
+        yaxis=dict(title_text="Count"),
+        barmode="stack",
+        paper_bgcolor = "cornsilk", 
+        font = {'color': "gray", 'family': "Arial", 'size':18},
+        plot_bgcolor='rgba(0,0,0,0)')
+
+    colors = ["gold", "greenyellow", "forestgreen"]
+    health_lev = ['Poor', 'Fair', 'Good']
+
+    for r, c in zip(health_lev, colors):
+        plot_df = df_steward[df_steward['health'] == r]
+        fig.add_trace( go.Bar(x=[plot_df.boroname, plot_df.steward], 
+                              y=plot_df.steward_x_health_counts, name=r, marker_color=c) )
+    return fig
 
 # %% formating NYC geojson data
 
@@ -162,6 +263,10 @@ fig_bkln = draw_boro_dial( boro_health_counts, 'Brooklyn' )
 fig_qns = draw_boro_dial( boro_health_counts, 'Queens' )
 fig_brnx = draw_boro_dial( boro_health_counts, 'Bronx' )
 fig_si = draw_boro_dial( boro_health_counts, 'Staten Island' )
+dial_legend = draw_dial_legend()
+steward_dat = steward_impact_data( dfff )
+fig_steward = steward_impact_plot( steward_dat )
+fig_steward2 = steward_numerosity_plot( steward_dat )
 
 
 
@@ -171,8 +276,12 @@ app.layout = html.Div([
                  style={'height':'40%', 'width':'40%'}), 
         style={'textAlign': 'center'}
         ),
-    html.H1('Tree Health NYC'),
-    
+    html.H1('Tree Health Applet for NYC', style={'color': 'green', 'fontSize': 40, 'textAlign': 'center'}),
+    html.Div([
+        html.P('Welcome arborists & tree enthusiasts!',style={'fontSize': 20, 'textAlign': 'center'}),
+        html.P("This tiny applet will enhance your understanding tree health in NYC. Just pick a tree species of interest from the dropdown menu to the left and explore the visualization panels",
+               style={'fontSize': 20, 'textAlign': 'center'})
+    ]),    
     
     html.Div([
             dcc.Dropdown(
@@ -180,10 +289,13 @@ app.layout = html.Div([
                 options=[{'label': i, 'value': i} for i in available_species],
                 value='crimson king maple'
             )
-        ], style={'width': '25%', 'display': 'inline-block'}),
+        ], style={'width': '25%', 'height':'5%', 'display': 'inline-block'}),
     
     dcc.Tabs([
         dcc.Tab( label = 'Health Dial', children= [
+            html.H2('Borough Health Dials', style={'color': 'green', 'fontSize': 30, 'textAlign': 'center'}),
+            html.P('Each dial shows the proportions of trees for the selected tree species that are in Good, Fair or Poor health for each NYC borough. The arrows point to and are colored the same as the majority health label.',
+                   style={'fontSize': 20, 'textAlign': 'center'}),
             html.Div([
                 # row for static boro dial plots
                 html.Div([
@@ -218,7 +330,7 @@ app.layout = html.Div([
                         figure=fig_brnx
                         )], 
                     style={'width': '32%', 'display': 'inline-block', 
-                           'margin' : '1px 1px 1px 250px'}),
+                           'margin' : '1px 1px 1px 1px'}),
             
                 html.Div([
                     dcc.Graph(
@@ -227,17 +339,51 @@ app.layout = html.Div([
                         )], 
                     style={'width': '32%', 'display': 'inline-block',
                            'margin' : '1px 1px 1px 1px'}),
+                
+                html.Div([
+                    dcc.Graph(
+                        id='graph6',
+                        figure=dial_legend
+                        )], 
+                    style={'width': '32%', 'display': 'inline-block',
+                           'margin' : '1px 1px 1px 1px'}),                
                 ], className='row')
             ]),
                      
             dcc.Tab( label = 'City View', children = [
+                html.H2('Street-level Tree Health', style={'color': 'green', 'fontSize': 30, 'textAlign': 'center'}),
+                html.P('Zoom in to find the health of the selected tree species in your very own neighborhood!',
+                       style={'fontSize': 20, 'textAlign': 'center'}),
                 html.Div([
                     # map plot. zoom in to see tree health in a certain location
                     html.Iframe( id = 'map', srcDoc = open('nycmap.html', 'r').read(), width = '100%', height='600')
                     ])
+                ]),
+            
+            dcc.Tab( label = 'Steward Impact', children = [
+                html.Div([
+                    html.H2('Impact of Stewards Tree Health in NYC', style={'color': 'green', 'fontSize': 30, 'textAlign': 'center'}),
+                    html.P('Here you can see the Percentage of trees with and without stewards to get a sense of whether stewards have an impact on the health of a species of tree across the boroughs.',
+                           style={'fontSize': 20, 'textAlign': 'center'}),
+                    dcc.Graph(
+                        id='graph7',
+                        figure=fig_steward
+                        )], 
+                    style={'width': '100%', 'height': '100%', 'display': 'inline-block', 
+                                           'margin' : '10px 10px 10px 10px'}),                
+                html.Div([
+                    html.P('Put the figure above in perspective by exploring the relative numerosity of trees in each category.',
+                           style={'fontSize': 20, 'textAlign': 'center'}),
+                    dcc.Graph(
+                        id='graph8',
+                        figure=fig_steward2
+                        )], 
+                    style={'width': '100%', 'height': '100%', 'display': 'inline-block', 
+                                           'margin' : '10px 10px 10px 10px'}), 
+                    ])
                 ])
-            ])
-    ])
+            ])    
+
     
     
 @app.callback(
@@ -247,6 +393,8 @@ app.layout = html.Div([
     Output('graph3', 'figure'),
     Output('graph4', 'figure'),
     Output('graph5', 'figure'),
+    Output('graph7', 'figure'),
+    Output('graph8', 'figure'),    
     Input('tree_species', 'value'))
 
 def update_graph(value):
@@ -261,7 +409,10 @@ def update_graph(value):
     fig_qns = draw_boro_dial( boro_health_counts, 'Queens' )
     fig_brnx = draw_boro_dial( boro_health_counts, 'Bronx' )
     fig_si = draw_boro_dial( boro_health_counts, 'Staten Island' )
-    return open( 'nycmap.html', 'r').read(), fig_man, fig_bkln, fig_qns, fig_brnx, fig_si
+    steward_dat = steward_impact_data( dff )
+    fig_steward = steward_impact_plot( steward_dat )
+    fig_steward2 = steward_numerosity_plot( steward_dat )
+    return open( 'nycmap.html', 'r').read(), fig_man, fig_bkln, fig_qns, fig_brnx, fig_si, fig_steward, fig_steward2
 
     
 
